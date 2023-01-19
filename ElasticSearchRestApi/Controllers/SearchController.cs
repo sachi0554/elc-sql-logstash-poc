89db0a7cc51685fb92866450432ee65561b6723e
Sachidanand <sachidanand@simformsolutions.com>
2023-01-19 16:46:43 +0530

    rest api added

---
+++ ElasticSearchRestApi/Controllers/SearchController.cs
@@ -0,0 +1,31 @@
+﻿using ElasticSearchPOC.POCO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+
+namespace ElasticSearchPOC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly IElasticClient _elasticClient;
+        private readonly ILogger<WeatherForecastController> _logger;
+        public SearchController(ILogger<WeatherForecastController> logger , IElasticClient elasticClient)
+        {
+            _logger = logger;
+            _elasticClient = elasticClient;
+        }
+        [HttpGet]
+        public async Task<IList<Product>> GetSearch(string keyword)
+        {
+            
+                var result = await _elasticClient.SearchAsync<Product>(s => s.Query(q => q.QueryString(d => d.Query('*' + keyword + '*'))).Size(100));
+                var finalResult = result;
+                var finalContent = finalResult.Documents.ToList();
+                return finalContent;
+            
+            
+        }
+    }
+}
---
+++ ElasticSearchRestApi/POCO/Product.cs
@@ -0,0 +1,18 @@
+﻿using Nest;
+
+namespace ElasticSearchPOC.POCO
+{
+    public class Product
+    {
+       
+        public int ProductID { get; set; }
+        public string ?Name { get; set; }
+        public string ?ProductNumber { get; set; }
+        public string ?Color { get; set; }
+        public decimal ?StandardCost { get; set; }
+        public decimal ?ListPrice { get; set; }
+        public int ?Size { get; set; }
+         
+        public int Weight { get; set; }
+    }
+}
